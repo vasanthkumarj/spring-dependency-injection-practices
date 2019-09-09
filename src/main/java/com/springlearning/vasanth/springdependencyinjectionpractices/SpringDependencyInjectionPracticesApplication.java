da460d373cc8cff18bb8cb97c5e5204f5405687d
vasanth <vasanthkumar.j@outlook.com>
2019-09-09 09:39:13 +0530

    Initial commit

---
+++ src/main/java/com/springlearning/vasanth/springdependencyinjectionpractices/SpringDependencyInjectionPracticesApplication.java
@@ -0,0 +1,13 @@
+package com.springlearning.vasanth.springdependencyinjectionpractices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDependencyInjectionPracticesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDependencyInjectionPracticesApplication.class, args);
+    }
+
+}
